0456c18df986d164d7975d133a1c6e174ea9e22e
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-11-29 04:58:27 -0200

    mamba: inicio

---
+++ logbook/mamba.sh
@@ -0,0 +1,20 @@
+###############################  logbook: mamba  ###############################
+# 2 vCPUs, 1GB RAM, 20GB SSD (CloudAtCost)
+# Ubuntu 16.04.2 LTS 64bit
+# mamba.kayen.ga (45.55.32.60)
+###############################################################################
+
+### Atalhos Rápidos
+## Acesso remoto
+# ssh root@mamba.kayen.ga
+#
+###
+
+### Preparação Inicial:
+## Atualiza sistema operacional
+sudo apt update
+sudo apt upgrade -y
+
+
+# Aplicações básicas. Não realmente essencial, mas torna vida mais simples
+sudo apt install mosh htop tree -y
