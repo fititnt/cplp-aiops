d88918b9971ab83678ac6e23bc95a5744442c241
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-05-15 22:58:10 -0300

    agua-pescadora (#5): CloudAtCost, reduz swap padrão para aumentar /boot (permite atualizar kernel); esses passos são extremamente especificos da CloudAtCost e da imagem padrao do Ubuntu 16.04.02; outros provedores provavelmente desnecessario essa etapa

--- logbook/aguia-pescadora.sh
+++ logbook/aguia-pescadora.sh
@@ -3,3 +3,62 @@
 # Ubuntu 16.04.2 LTS 64bit
 # (104.167.109.226)
 ################################################################################
+
+
+#### Preparação Inicial:
+
+# Aviso: a recomendação a seguir sobre 'Swap + /boot' é extremanente especifica
+# para uso na CloudAtCost. Outros provedores de VPSs tipicamente não requerem.
+
+### Swap + /boot, inicio
+## CloudAtCost, reduz swap padrão para aumentar /boot (permite atualizar kernel)
+## @see http://www2.fugitol.com/2012/04/linux-resizing-boot-partition.html
+## @see http://tabletuser.blogspot.com/2017/12/ubuntu-1604-resize-boot-partition-for.html (alternativa)
+
+# Requisito previo: setar label na swap (por padrão vem sem label)
+swaplabel -L SWAP-sda2 /dev/sda2
+
+# Então seguir com os passos de http://www2.fugitol.com/2012/04/linux-resizing-boot-partition.html
+fdisk -l
+blkid /dev/sda1 /dev/sda2
+swapoff -a
+umount /boot
+tune2fs -O ^has_journal /dev/sda1
+# ***Siga os passos 6 até 13*** do site
+# Nota: no passo 8, em vez de "42" escolha "2181119" (sem aspas)
+
+# Nota: ao final do passo 12, deve ter algo como
+#   Device     Boot    Start      End  Sectors  Size Id Type
+#   /dev/sda1           2048  2181118  2179071    1G 83 Linux
+#   /dev/sda2        2181119  4362239  2181121    1G 82 Linux swap / Solaris
+#   /dev/sda3        4362240 20969471 16607232  7.9G 8e Linux LVM
+#   /dev/sda4       20969472 40959999 19990528  9.5G 8e Linux LVM
+
+# Nota: é ok aparecer o erro em vermelho "Re-reading the partition table failed.: Device or resource busy"
+
+# Checa se o disco esta ok
+e2fsck -f /dev/sda1
+
+# Reinicar
+reboot
+
+# SSH no servidor...
+
+# Reabilitar swap e terminar o processo
+swapoff -a
+mkswap -L SWAP-sda2 /dev/sda2
+# swapon -a
+swapon /dev/sda2 # Alterado para deixar explicito o lugar
+umount /boot
+e2fsck -f /dev/sda1 # Comando extra em relação a documentação
+resize2fs /dev/sda1
+tune2fs -j /dev/sda1
+mount /boot
+
+## Caso swap falhe no reboot teste editar /etc/fstab e trocar linhas deixando UUUID=... explicito a /dev/sda2 Ex.:
+## UUID=c9f61b59-95aa-45ab-ae18-ee968312edc4 none            swap    sw              0       0
+# /dev/sda2       none            swap    sw              0       0
+
+#
+##
+### Swap + /boot, fim
