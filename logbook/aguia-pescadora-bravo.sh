d6ff573d4dd816948ec98416960889a064dfd56c
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-05-18 00:49:37 -0300

    aguia-pescadora-bravo (#16): Configuração inicial (Primeiro login, Atualizar sistema operaciona, Define hostname personalizado)

--- logbook/aguia-pescadora-bravo.sh
+++ logbook/aguia-pescadora-bravo.sh
@@ -29,3 +29,33 @@ exit
 #   developers look. I'm saying this in special for people who help over
 #   Facebook discussions. Even the ones without a personal computer yet.
 ################################################################################
+
+#------------------------------------------------------------------------------#
+# SEÇÃO: Configuração inicial                                                  #
+# TL;DR: Isso é feito ao receber uma VPS do zero                               #
+#------------------------------------------------------------------------------#
+
+### Primeiro login______________________________________________________________
+# Você, seja por usuario + senha, ou por Chave SSH, normlamente terá que acessar
+# diretamente como root. Excessões a esta regra (como VMs na AWS) geralmente
+# implicam em logar em um usuario não-root e executar como sudo. Mas nesta da
+# é por root mesmo nesse momento.
+ssh root@192.99.247.117
+
+### Atualizar o sistema operacional_____________________________________________
+# O sistema operacional (neste caso, Ubuntu) normalmente não vai estar 100%
+# atualizado. Os comandos abaixo são uma boa prática pra fazer imediatamente
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+### Define um hostname personalizado____________________________________________
+# O hostname padrão geralmente não faz muito sentido pra você e os usuparios
+# da VPS. No nosso caso, era vps240016, como em root@vps240016.
+# Nota: o domínio 'aguia-pescadora-bravo.etica.ai' já aponta para 192.99.247.117
+sudo hostnamectl set-hostname aguia-pescadora-bravo.etica.ai
+
+# Edite /etc/hosts e adicione o hostname também apontando para 127.0.0.1
+sudo vi /etc/hosts
+## Adicione
+# 127.0.0.1 aguia-pescadora-bravo.etica.ai  aguia-pescadora-bravo
