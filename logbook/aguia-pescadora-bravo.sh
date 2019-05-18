4130a9044f9b1bce8d85883c61c3c92ad374af50
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-05-17 22:53:42 -0300

    aguia-pescadora-bravo (#16): iniciado documentação do setup do servidor bravo

---
+++ logbook/aguia-pescadora-bravo.sh
@@ -0,0 +1,31 @@
+echo "Este diário de bordo foi feito para ser visualizado, nao executado assim!"
+exit
+
+#################### Diario de bordo: aguia-pescadora-bravo ####################
+# VPS (KVM), 2 vCPUs, 8GB RAM, 80GB SSD, Ubuntu Server 18.04 64bit, CloudAtCost (Canada)
+#
+# Datacenter: CloudAtCost, Canada
+# Type: Virtual Machine, KVM
+# OS: Ubuntu Server 18.04 LTS 64bit
+# CPU: 2vCPUs
+# RAM: 7786MB
+# Disk: 80GB
+#
+# IPv4: 192.99.247.117
+# IPv6: 2607:5300:201:3100:0:0:0:87fc
+# Domain:
+#   Full: aguia-pescadora-bravo.etica.ai (apb.etica.ai)
+#   Short: apb.etica.ai
+#
+# -----------------------------------------------------------------------------#
+# LICENSE: Public Domain
+#   Except where otherwise noted, content on this server configuration and to
+#   the extent possible under law, Emerson Rocha has waived all copyright and
+#   related or neighboring rights to this work to Public Domain
+#
+# MAINTAINER: Emerson Rocha <rocha(at)ieee.org>
+#   Keep in mind that several people help with suggestions, bugfixes and
+#   inspiration and inspire without get names noted in places that software
+#   developers look. I'm saying this in special for people who help over
+#   Facebook discussions. Even the ones without a personal computer yet.
+################################################################################
