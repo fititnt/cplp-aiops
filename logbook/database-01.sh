b4b87b50bd37f0d8396f17f432191fd805959580
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-05-25 13:43:54 -0300

    database {01,02,03}: iniciado

---
+++ logbook/database-01.sh
@@ -0,0 +1,15 @@
+echo "Este diário de bordo foi feito para ser visualizado, nao executado assim!"
+exit
+######################### Diario de bordo: database-01 #########################
+# VPS (KVM), 1 vCPU, 2GB RAM, 20GB SSD, Ubuntu Server 18.04 64bit, OVH, Canada
+#
+# Datacenter: OVH, Canada
+# Type: Virtual Machine, KVM
+# OS: Ubuntu Server 18.04 LTS 64bit
+# CPU: 1 vCPU
+# RAM: 2 GB
+# Disk: 20 GB
+#
+# IPv4: 149.56.130.19
+# IPv6: 2607:5300:0201:3100:0000:0000:0000:2f58
+################################################################################
