787a8002535a3b93a72ad8210d77bad15947a28f
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-05-26 09:08:53 -0300

    elefante-borneu-yul{01,02,03} (#45): instalado vnstat; apontado subdominios

--- logbook/elefante-borneu-yul-03.sh
+++ logbook/elefante-borneu-yul-03.sh
@@ -12,6 +12,35 @@ exit
 #
 # IPv4: 149.56.130.178
 # IPv6: 2607:5300:0201:3100:0000:0000:0000:6df7
+#
+# Domain:
+#   Full: elefante-borneu-yul-03.etica.ai (TTL: 15 min)
+#   Short: ebyul03.etica.ai (TTL: 15 min)
+#
+# Domain, extras:
+#   lb-ebyul.etica.ai (TTL: 2 min)
+#   lb-eb.etica.ai (TTL: 2 min)
+#
+# Login:
+#   ssh user@elefante-borneu-yul-03.etica.ai
+#   mosh user@elefante-borneu-yul-03.etica.ai (TODO: install Mosh)
+#   ssh user@ebyul03.etica.ai
+#   mosh user@ebyul03.etica.ai (TODO: install Mosh)
+#
+# -----------------------------------------------------------------------------#
+# LICENSE: Public Domain
+#   Except where otherwise noted, content on this server configuration and to
+#   the extent possible under law, Emerson Rocha has waived all copyright and
+#   related or neighboring rights to this work to Public Domain
+#
+# MAINTAINER: Emerson Rocha <rocha(at)ieee.org>
+#   Keep in mind that several people help with suggestions, bugfixes and
+#   inspiration and inspire without get names noted in places that software
+#   developers look. I'm saying this in special for people who help over
+#   Facebook discussions. Even the ones without a personal computer yet.
+# SECURITY:
+#   Reporting a Vulnerability:
+#   Send e-mail to Emerson Rocha: rocha(at)ieee.org.
 ################################################################################
 
 #------------------------------------------------------------------------------#
@@ -71,3 +100,30 @@ cat /proc/sys/vm/vfs_cache_pressure
 
 vim /etc/sysctl.conf
 # Adciona 'vm.vfs_cache_pressure=50' (sem aspas) no final do arquivo
+
+##### VNstat [configuração inicial] ____________________________________________
+# VNstat é uma ferramenta super eficiente que permite monitorar uso de rede
+# ao longo do mês
+# @see https://www.howtoforge.com/tutorial/vnstat-network-monitoring-ubuntu/
+
+sudo apt install vnstat vnstati
+
+# O comando a seguir deve ser usado para decidir qual a interface a monitorar
+ifconfig
+# Escolha a network para iniciar monitoramento. Neste caso é ens3
+
+vnstat -u -i ens3
+
+## vnstat --iflist
+# Available interfaces: ens3 lo
+
+# O arquivo /etc/vnstat.conf deve estar marcado para nossa interface desejada
+sudo vim /etc/vnstat.conf
+# trocar eth0 pela network especifica
+# default interface
+# Interface "ens3"
+# Para ver em tempo real o uso de rede
+
+# Vamos iniciar vnstat para rodar imediatamente, e também iniciar com sistema
+sudo systemctl start vnstat
+sudo systemctl enable vnstat
