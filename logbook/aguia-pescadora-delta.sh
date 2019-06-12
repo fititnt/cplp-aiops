39d69eb7673a4eb832d3370e3b85462f31f2320b
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-06-12 08:37:40 -0300

    aguia-pescadora-delta (#63): iniciado

---
+++ logbook/aguia-pescadora-delta.sh
@@ -0,0 +1,35 @@
+echo "Este diário de bordo foi feito para ser visualizado, nao executado assim!"
+exit
+
+################### Diario de bordo: aguia-pescadora-delta ###################
+# VPS (KVM), 6 vCPUs, 16GB RAM, 400GB SSD, Ubuntu Server 18.04 64bit, Contabo (Germany)
+#
+# Datacenter: Contabo, Germany
+# Type: Virtual Machine, KVM
+# OS: Ubuntu Server 18.04 LTS 64bit
+# CPU: 6 vCPUs
+# RAM: 16040 MB
+# Disk: 400 GB
+#
+# IPv4: 173.249.10.99
+# IPv6: 
+# Domain:
+#   Full: aguia-pescadora-delta.etica.ai
+#   Short: apd.etica.ai
+#
+#
+# -----------------------------------------------------------------------------#
+# LICENSE: Public Domain
+#   Except where otherwise noted, content on this server configuration and to
+#   the extent possible under law, Emerson Rocha has waived all copyright and
+#   related or neighboring rights to this work to Public Domain
+#
+# MAINTAINER: Emerson Rocha <rocha(at)ieee.org>
+#   Keep in mind that several people help with suggestions, bugfixes and
+#   inspiration and inspire without get names noted in places that software
+#   developers look. I'm saying this in special for people who help over
+#   Facebook discussions. Even the ones without a personal computer yet.
+# SECURITY:
+#   Reporting a Vulnerability:
+#   Send e-mail to Emerson Rocha: rocha(at)ieee.org.
+################################################################################
