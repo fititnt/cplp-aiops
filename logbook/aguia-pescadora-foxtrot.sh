9a06fc65d37eed2f15e2f45ae60771b4f4a5675c
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-06-17 02:44:31 -0300

    aguia-pescadora-{delta,echo,foxtrot} (#63, #64, #65): public key (superadmin e Tsuru); anotações sobre ser necessario pesquisar melhores praticas relacionadas a usar ou não Swap em nó do Tsuru

--- logbook/aguia-pescadora-foxtrot.sh
+++ logbook/aguia-pescadora-foxtrot.sh
@@ -71,3 +71,52 @@ sudo timedatectl set-timezone UTC
 
 ### Criar Swap & ajusta Swappiness______________________________________________
 ## TODO: setup swap from 2GB (defalt from Contabo) to 8GB (fititnt, 2019-06-16 01:44 BRT)
+
+## Já temos uma Swap de 2GB
+# root@aguia-pescadora-delta:/# ls -lh /swapfile
+# -rw------- 1 root root 2.0G Jun 12 11:05 /swapfile
+
+## @see https://bogdancornianu.com/change-swap-size-in-ubuntu/
+## @see https://askubuntu.com/questions/1075505/how-do-i-increase-swapfile-in-ubuntu-18-04/1075516#1075516
+
+## Cria um /swapfile de 4GB
+# @see https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-18-04
+# @see https://askubuntu.com/questions/1075505/how-do-i-increase-swapfile-in-ubuntu-18-04/1075516#1075516
+
+# TODO: descobrir configurações para rodar Tsuru em produção que estejam
+#       relacionadas a swap. Numa olhada rápida ao menos quando usado
+#        Kubernetes (que NÃO é nosso caso) os desenvolvedores desativam
+#        funcionamento em sistemas com Swap sem parametro especial, vide
+#        https://github.com/kubernetes/kubernetes/issues/53533
+#        (fititnt, 2019-06-17 02:16 BRT)
+
+### Adiciona chave (pelo menos um super admin) _________________________________
+## Cheque se já existe o arquivo authorized_keys
+ls -lha /root/.ssh/authorized_keys
+
+## Caso não exista, crie
+sudo mkdir -p /root/.ssh/
+sudo chmod 700 /root/.ssh/
+sudo touch /root/.ssh/authorized_keys
+sudo chmod 600 /root/.ssh/authorized_keys
+
+## Troque para chave publica de cada um dos administradores, o arquivo PUB
+#    cat ~/.ssh/id_rsa.pub
+# Neste caso usado para inicializar ao menos o
+#   cat ~/.ssh/id_rsa-rocha-eticaai-2019.pub
+sudo echo "ssh-rsa (...chave...)== email@dominio.tdl" >> /root/.ssh/authorized_keys
+
+## Reveja as chaves em /root/.ssh/authorized_keys e tenha certeza que esta tudo
+## como deveria
+sudo cat /root/.ssh/authorized_keys
+
+#------------------------------------------------------------------------------#
+# SEÇÃO TSURU: ADIÇÃO DA CHAVE SSH PARA SER CONFIGURADO REMOTAMENTE            #
+#                                                                              #
+# AVISO: veja devel-fititnt-bravo.sh para saber como a chave foi criada        #
+#------------------------------------------------------------------------------#
+sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7erwMfyTSO7xn8axjAp2NTbBHjDVdu+6J17ZjX3Rs55dy3Vsqmq4kBIq3qxShabfY6h5nW3ccc86hGy8coXjPCblyloAKlG0RKkRo7/sGjsl3jv8i0gZVLU/H8pjaLLGhRWca2ToJAPJTlnFk/VrCMvH6PCHca7X70j88uE6UR5W1nax94kzcyOf/65mQDx7dHYVVyBL+Rgn0CHS4Di8Z0PSbwn1dVA0S4JW1z1DZ/5AYdhOBCfPkDvj4trTr9lmJIn/6KnOX+MIMzViHtxZw3dg8VHcZxd2PeiJ/THZZ3Z34Bv60jEwyjZMNKB6fqz4mAGkHH8bAXMS4m6gZXw6TaPZk84x3t9rJnzWhPaUYOkPL9dgcZ8m+FmeUxKkJgdo10AqZAMVdboYEKhL4Uv9JvZrt/VdkM6C2FqIDEddm6TWnqZiteeLtCl0EU5PMxsfQUncHkRihya6R1Brysu5lvTGEvW1qoobONowT3ED2F5aDTPlyscTr4ogKXAJda+jI5oIGxkf2QaKzhdJlt76KktQRVlOQVYJeKcVOB853IVMSJvIpP09YReaibrxdSYeazu+SswqNK7ux7S3Xb82PtSu7jtJtiiCdU6zfCLkWPAmoqP8N3m1q2lw4VvXxvLeUp79n3cv+kabG0UpE2csyJArSX/eyUF7+6F9QWQo4ow== aguia-pescadora-tsuru.no-reply@etica.ai" >> ~/.ssh/authorized_keys
+
+## Reveja as chaves em /root/.ssh/authorized_keys e tenha certeza que esta tudo
+## como deveria
+sudo cat /root/.ssh/authorized_keys
